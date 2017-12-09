e109892bfe584eec8597fea0018547775bbe8ee2
Tor Hagland <torhagl@gmail.com>
2017-12-09 13:29:05 +0100

    Added bootstrap cards and stuff so now it views stuff from the database on the cards. Search works only for towns at the moment, which is fine. Think I'll call it done for now, not much else to do other than designshit at this point

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,9 @@ import Regex
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Form.Input as Input
+import Bootstrap.Card as Card
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
 import String.Extra exposing (..)
 import Json.Decode exposing (decodeString)
 import JsonDecoder exposing (welcome, Welcome, Feature)
@@ -81,28 +84,53 @@ view : Model -> Html Msg
 view model =
     Grid.container []
         [ CDN.stylesheet
-        , div [ style [ ( "margin-top", "2em" ) ] ]
-            [ Input.text
-                [ Input.placeholder "Start typing!"
-                , Input.onInput Input
+        , Grid.row []
+            [ Grid.col []
+                [ div [ style [ ( "margin-top", "2em" ), ( "margin-bottom", "0.5em" ) ] ]
+                    [ Input.text
+                        [ Input.placeholder "Search for a town or something!"
+                        , Input.onInput Input
+                        ]
+                    ]
                 ]
             ]
-        , div [ style [ ( "margin-top", "0.5em" ) ] ]
-            [ if model.input == "" then
-                text "Results come here!"
-              else
-                searchFilterTowns model.input model.db
+        , Grid.row [ Row.middleLg ] <| List.map createToiletCard <| regexTowns model.input model.db.features
+        ]
 
-            --text getJson
-            ]
+
+createToiletCard : Feature -> Grid.Column Msg
+createToiletCard toilet =
+    Grid.col [ Col.md6 ]
+        [ Card.config [ Card.outlineInfo ]
+            |> Card.headerH5 [] [ text toilet.purpleProperties.lOCATION ]
+            |> Card.imgBottom [ src toilet.purpleProperties.lINKTOPHOTO, style [ ( "width", "100%" ), ( "height", "40%" ), ( "float", "left" ) ], alt "Photo of the toilet." ] []
+            |> Card.block []
+                [ Card.text [] [ text <| "Address: " ++ toilet.purpleProperties.sTREET ++ ", " ++ toilet.purpleProperties.tOWN ]
+                , Card.text [] [ text <| "Managed by: " ++ toilet.purpleProperties.mANAGEDBY ]
+                , Card.text [] [ text <| "Status: " ++ toilet.purpleProperties.sTATUS ]
+                , Card.text [] [ text <| "Used: " ++ toilet.purpleProperties.uSED ]
+                , Card.text []
+                    [ if toilet.purpleProperties.sIZE == " " then
+                        text <| "Size: Not specified."
+                      else
+                        text <| "Size: " ++ toilet.purpleProperties.sIZE
+                    ]
+                , Card.text []
+                    [ if toilet.purpleProperties.cOMMENTS == " " then
+                        text <| "Comments: None"
+                      else
+                        text <| "Comments: " ++ toilet.purpleProperties.cOMMENTS
+                    ]
+                ]
+            |> Card.view
         ]
 
 
 searchFilterTowns : String -> Welcome -> Html Msg
 searchFilterTowns input db =
-    div [] (List.map (\y -> div [] [ Html.text <| toSentenceCase y.purpleProperties.tOWN ]) <| regexTowns input db.features)
+    div [] (List.map (\y -> div [] [ Html.text <| toSentenceCase y.purpleProperties.tOWN, Html.text <| toSentenceCase y.purpleProperties.cOMMENTS ]) <| regexTowns input db.features)
 
 
-regexTowns : String -> List Feature -> List JsonDecoder.Feature
+regexTowns : String -> List Feature -> List Feature
 regexTowns input toilets =
     List.filter (\y -> Regex.contains (Regex.caseInsensitive <| Regex.regex <| "^" ++ input) y.purpleProperties.tOWN) toilets
